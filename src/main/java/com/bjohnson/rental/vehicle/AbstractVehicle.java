6549b319f5a99e47a68b2403b49bc963718fecb3
Bobby Johnson <rjohnson19@gmail.com>
2018-06-27 10:44:36 -0400

    Cleaned up unused methods in AbstractVehicle.

--- src/main/java/com/bjohnson/rental/vehicle/AbstractVehicle.java
+++ src/main/java/com/bjohnson/rental/vehicle/AbstractVehicle.java
@@ -48,6 +48,7 @@ public abstract class AbstractVehicle implements Vehicle {
 
     /**
      * Return the base rental cost per day of this vehicle.
+     *
      * @return Cost per day of this vehicle. Can be 0 but not negative.
      */
     protected abstract int getCostPerDay();
@@ -56,6 +57,7 @@ public abstract class AbstractVehicle implements Vehicle {
      * Return the effective quantity for cost calculation for this vehicle rental.
      * This allows subclasses to adjust the billed quantity based on factors such as
      * the other vehicles behind rented.
+     *
      * @param otherVehicles List of other vehicles being rented by the renter.
      * @return The effective quantity to be used for calculating cost.
      */
@@ -64,39 +66,17 @@ public abstract class AbstractVehicle implements Vehicle {
     }
 
     /**
-     * Calculate the cost for renting this vehicle, given its cost per day and the current day.
-     * This cost includes modifiers added to the vehicle as options.
-     * @param dayOfWeek Current day of week.
-     * @return Cost of renting this vehicle (in desired quantity) for the given day.
-     */
-    protected float calculateDayStandardCost(DayOfWeek dayOfWeek, List<Vehicle> otherVehicles) {
-        float myCost = calculateBaseCost(otherVehicles);
-        // apply any per day cost, and fixed fee modifiers to each vehicle being rented
-        myCost = addModifierDayCosts(dayOfWeek, myCost, otherVehicles);
-
-        return myCost;
-    }
-
-    /**
-     * Calculate the absolute base cost of renting this vehicle given its cost per day.
-     * DOES NOT include any modifiers.
-     * @return Base cost of renting this vehicle (in desired quantity).
-     */
-    protected int calculateBaseCost(List<Vehicle> otherVehicles) {
-        return getCostPerDay() * getEffectiveQuantity(otherVehicles);
-    }
-
-    /**
      * Calculates the cost associated with modifiers applied to this rental, specific to a particular day.
+     *
      * @param dayOfWeek Day of week vehicle is being rented.
-     * @param myCost The current base cost of the vehicle.
+     * @param myCost    The current base cost of the vehicle.
      * @return Updated cost, accounting for any modifiers applied.
      */
     protected float addModifierDayCosts(DayOfWeek dayOfWeek, float myCost, List<Vehicle> otherVehicles) {
         final int effectiveQuantity = getEffectiveQuantity(otherVehicles);
         for (Modifier modifier : getApplicableModifiers()) {
-                float perDayModifierCost = modifier.getPerDayCost(dayOfWeek) * effectiveQuantity;
-                myCost += perDayModifierCost;
+            float perDayModifierCost = modifier.getPerDayCost(dayOfWeek) * effectiveQuantity;
+            myCost += perDayModifierCost;
         }
         return myCost;
     }
@@ -115,8 +95,9 @@ public abstract class AbstractVehicle implements Vehicle {
     /**
      * Applies premiums or discounts to the cost that occur based on the day of week a vehicle is being rented.
      * The default implementation in AbstractVehicle makes no changes to the cost.
+     *
      * @param dayOfWeek DayOfWeek rental is occuring.
-     * @param myCost The current cost of renting this vehicle.
+     * @param myCost    The current cost of renting this vehicle.
      * @return Adjusted cost of rental based on the current day of the week.
      */
     protected float applyDayBasedModifications(DayOfWeek dayOfWeek, float myCost) {
